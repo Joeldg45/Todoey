a30d0c7a38e6908a2a4e804519cef8f0a7435469
Joel Georges <georges1joel0@gmail.com>
2019-05-29 12:47:29 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Joel Georges on 5/29/19.
+//  Copyright © 2019 Joel Georges. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
