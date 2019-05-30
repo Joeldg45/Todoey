09bb7652cf9f1de11a4c407d0c36b64c6d241587
Joel Georges <georges1joel0@gmail.com>
2019-05-30 16:30:23 -0400

    Persisting data using core data

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,121 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Joel Georges on 5/29/19.
+//  Copyright © 2019 Joel Georges. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        
+        loadCategories()
+        
+    }
+    
+    //MARK - Tableview DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categories.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+        
+    }
+    
+    //MARK - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow{
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
+    
+    
+
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            //what will happen once the user clicks the Add item button on UIAlert
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            
+            self.saveCategories()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //MARK - Model MAnipulation Methods
+    //create
+    func saveCategories(){
+        
+        
+        do{
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    //update
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()){
+        
+        do {
+            categories = try context.fetch(request)
+        }catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+}
